79b381a30b6f7581cb6a018faddf616ce63060e7
David Tenenbaum <github email address>
2018-07-10 13:24:10 -0400

    added fetch and some css

--- src/index.js
+++ src/index.js
@@ -1,45 +1,41 @@
-const possibleLetters = ["R", "I", "F", "O", "B", "X", "I", "F", "E", "H", "E", "Y", "D", "E", "N", "O", "W", "S", "U", "T",
-"O", "K", "N", "D", "H", "M", "S", "R", "A", "O", "L", "U", "P", "E", "T", "S", "A", "C", "I", "T",
-"O", "A", "Y", "L", "G", "K", "U", "E", "Qu", "B", "M", "J", "O", "A", "E", "H", "I", "S", "P", "N",
-"V", "E", "T", "I", "G", "N", "B", "A", "L", "I", "Y", "T", "E", "Z", "A", "V", "N", "D", "R", "A",
- "L", "E", "S", "C", "U", "W", "I", "L", "R", "G", "P", "A", "C", "E", "M", "D"]
+document.addEventListener('DOMContentLoaded', function() {
+  const possibleLetters = ["R", "I", "F", "O", "B", "X", "I", "F", "E", "H", "E", "Y", "D", "E", "N", "O", "W", "S", "U", "T", "O", "K", "N", "D", "H", "M", "S", "R", "A", "O", "L", "U", "P", "E", "T", "S", "A", "C", "I", "T", "O", "A", "Y", "L", "G", "K", "U", "E", "Qu", "B", "M", "J", "O", "A", "E", "H", "I", "S", "P", "N", "V", "E", "T", "I", "G", "N", "B", "A", "L", "I", "Y", "T", "E", "Z", "A", "V", "N", "D", "R", "A", "L", "E", "S", "C", "U", "W", "I", "L", "R", "G", "P", "A", "C", "E", "M", "D"]
 
-gameArray = [];
-for (let i = 0; i < 16; i++){
-  gameArray.push(possibleLetters[Math.floor(Math.random()*possibleLetters.length)]);
-}
+  const boxes = document.getElementsByClassName('box')
+  const newBoardButton = document.getElementById('new-board-button')
 
-// const box0 = document.getElementById('box0')
-// const box1 = document.getElementById('box1')
-// const box2 = document.getElementById('box2')
-// const box3 = document.getElementById('box3')
-// const box4 = document.getElementById('box4')
-// const box5 = document.getElementById('box5')
-// const box6 = document.getElementById('box6')
-// const box7 = document.getElementById('box7')
-// const box8 = document.getElementById('box8')
-// const box9 = document.getElementById('box9')
-// const box10 = document.getElementById('box10')
-// const box11 = document.getElementById('box11')
-// const box12 = document.getElementById('box12')
-// const box13 = document.getElementById('box13')
-// const box14 = document.getElementById('box14')
-// const box15 = document.getElementById('box15')
-const boxes = document.getElementsByClassName('box')
-function startNewGame(){
-  populateBoard()
-}
 
-function populateBoard() {
-  let i = 0
-  Array.from(boxes).forEach(function(box) {
-    box.innerText = gameArray[i]
-    i++
+  newBoardButton.addEventListener('click', function() {
+    const newBoard = populateBoard()
+
+    let i = 0;
+    Array.from(boxes).forEach(function(box) {
+      box.innerText = newBoard[i];
+      i++;
+    })
+
+    fetch(`http://api.codebox.org.uk/boggle/${newBoard.join("")}`)
+      .then(res => res.json())
+      .then(data => parseData(data))
   })
-}
+
+  function populateBoard() {
+    const board = []
+    for (let i = 0; i < 16; i++){
+      board.push(possibleLetters[Math.floor(Math.random()*possibleLetters.length)]);
+    }
+    return board;
+  }
+
+  function parseData(data) {
+    const possibleWords = []
+    data.forEach(function(word) {
+      possibleWords.push(word);
+    })
+    console.log(possibleWords)
+  }
 
 
 
 
-
-startNewGame()
+});
