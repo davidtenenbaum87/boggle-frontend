6572ee10f56ba670c2b61805d8ae3b418cb6fe2a
David Tenenbaum <github email address>
2018-07-10 17:48:46 -0400

    added timer and score

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const possibleLetters = ["R", "I", "F", "O", "B", "X", "I", "F", "E", "H", "E", "Y", "D", "E", "N", "O", "W", "S", "U", "T", "O", "K", "N", "D", "H", "M", "S", "R", "A", "O", "L", "U", "P", "E", "T", "S", "A", "C", "I", "T", "O", "A", "Y", "L", "G", "K", "U", "E", "Qu", "B", "M", "J", "O", "A", "E", "H", "I", "S", "P", "N", "V", "E", "T", "I", "G", "N", "B", "A", "L", "I", "Y", "T", "E", "Z", "A", "V", "N", "D", "R", "A", "L", "E", "S", "C", "U", "W", "I", "L", "R", "G", "P", "A", "C", "E", "M", "D"]
+  const possibleLetters = ["R", "I", "F", "O", "B", "X", "I", "F", "E", "H", "E", "Y", "D", "E", "N", "O", "W", "S", "U", "T", "O", "K", "N", "D", "H", "M", "S", "R", "A", "O", "L", "U", "P", "E", "T", "S", "A", "C", "I", "T", "O", "A", "Y", "L", "G", "K", "U", "E", "Q", "B", "M", "J", "O", "A", "E", "H", "I", "S", "P", "N", "V", "E", "T", "I", "G", "N", "B", "A", "L", "I", "Y", "T", "E", "Z", "A", "V", "N", "D", "R", "A", "L", "E", "S", "C", "U", "W", "I", "L", "R", "G", "P", "A", "C", "E", "M", "D"]
 
   const boxes = document.getElementsByClassName('box')
   const newBoardButton = document.getElementById('new-board-button')
   const wordList = document.getElementById('wordList')
+  const scoreBox = document.getElementById('score')
+
   const foundWords = [];
   const possibleWords = []
+  let score = 0;
 
 // event listener for creating a new board
   newBoardButton.addEventListener('click', function() {
     const newBoard = populateBoard()
+    document.getElementById('timer').innerHTML =
+      03 + ":" + 00;
+    score = 0;
+    displayScore()
     wordList.innerHTML = ""
     let i = 0;
     Array.from(boxes).forEach(function(box) {
@@ -50,18 +57,100 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
-// adding the words to the HTML
-  function addWordToList(word) {
-    wordList.innerHTML += `<li> ${word} </li>`
-  }
-
-
+// checking if the input word exists in the api
   function checkWord(word) {
-    if (possibleWords.includes(word.toUpperCase())) {
-      addWordToList(word)
+    const currentWord = word.toUpperCase();
+    const correctWords = []
+    if (possibleWords.includes(currentWord)) {
+      if (!correctWords.includes(currentWord)) {
+        correctWords.push(currentWord);
+        addWordToList(word);
+        keepScore(currentWord);
+      }
     } else {
       console.log("not a word")
     }
   };
+
+  // adding the words to the HTML
+    function addWordToList(word) {
+      wordList.innerHTML += `<li> ${word} </li>`
+    }
+
+  function keepScore(word) {
+    if (word.length < 5) {
+      score += 1;
+    } else if (word.length === 5) {
+      score += 2;
+    } else if (word.length === 6) {
+      score += 3;
+    } else if (word.length === 7) {
+      score += 5;
+    } else {
+      score += 11;
+    }
+    displayScore()
+  }
+
+  function displayScore() {
+    scoreBox.innerText = `Score: ${score}`
+  }
+
+document.getElementById('timer').innerHTML =
+  03 + ":" + 00;
+startTimer();
+
+function startTimer() {
+  var presentTime = document.getElementById('timer').innerHTML;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond((timeArray[1] - 1));
+  if(s==59){m=m-1}
+  //if(m<0){alert('timer completed')}
+
+  document.getElementById('timer').innerHTML =
+    m + ":" + s;
+  setTimeout(startTimer, 1000);
+}
+
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+  if (sec < 0) {sec = "59"};
+  return sec;
+}
+
+
+
+
+
+//   function setTimer() {
+//     timer = ({ timer }) => {
+//
+//     const minute = () => {
+//      return Math.floor(timer / 60)
+//     }
+//
+//  const second = () => {
+//    let s = timer % 60;
+//    return (s<10) ? `0${s}` : s
+//  }
+//
+//  return(
+//    <div className="boggle-timer">
+//      <div id="hour-glass">
+//        <div id="glass"></div>
+//        <div id="sand-stream"></div>
+//        <div id="top-sand"></div>
+//        <div id="bottom-sand"></div>
+//      </div>
+//    </div>
+//  )
+//
+// }
+//
+// export default Timer;
+//   }
+
+
 
 });
